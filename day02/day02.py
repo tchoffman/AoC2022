99b317c598788d22bbf92d78d2c3b99a99443477
Trenton Hoffman <trenton.hoffman@elemica.com>
2022-12-02 14:12:42 -0500

    day2 part 2 complete

--- day02/day02.py
+++ day02/day02.py
@@ -1,5 +1,6 @@
 moves = {"A": "R", "X": "R", "B": "P", "Y": "P", "C": "S", "Z": "S"}
 points = {"R": 1, "P": 2, "S": 3}
+outcomes = {"X": 1, "Y": 2, "Z": 0}
 
 
 def rpc_result(opponent, player):
@@ -20,3 +21,24 @@ with open('input.txt') as input_data:
         total += sum(score)
 
 print("Part1: ", total)
+
+
+def get_plr_move(opponent, outcome):
+    o = points[opponent]
+    p = (o + outcome) % 3
+    return ["R", "P", "S"][p]
+
+
+total = 0
+
+with open('input.txt') as input_data:
+    for line in input_data.readlines():
+        opp = moves[line[0]]
+        out = outcomes[line[2]]
+
+        plr = get_plr_move(opp, out)
+        print(opp, line[2], plr)
+        score = rpc_result(opp, plr)
+        total += sum(score)
+
+print("Part2: ", total)
