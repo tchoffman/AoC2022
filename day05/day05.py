60cfd95c2446374f79552da3f49e9e5366fee5b4
Trenton Hoffman <trenton.hoffman@elemica.com>
2022-12-05 09:35:44 -0500

    day5 partB complete

--- day05/day05.py
+++ day05/day05.py
@@ -20,13 +20,27 @@ with open('input.txt') as input_data:
         stack.reverse()
 
     #print(commands)
-    print(stacks)
+    for s in stacks:
+        print(s)
+    # for cmd in commands:
+    #     for i in range(cmd['n']):
+    #         i = cmd['dest'] - 1
+    #         j = cmd['loc'] - 1
+    #         stacks[i].append(stacks[j].pop())
+    # print("Part1:", "".join([i[-1] for i in stacks]))
+
     for cmd in commands:
-        for i in range(cmd['n']):
-            i = cmd['dest'] - 1
-            j = cmd['loc'] - 1
-            stacks[i].append(stacks[j].pop())
-    print("".join([i[-1] for i in stacks]))
+        i = cmd['dest'] - 1
+        j = cmd['loc'] - 1
+        moving = []
+        for c in range(cmd['n']):
+            moving.append(stacks[j].pop())
+        moving.reverse()
+        print(cmd, moving)
+        stacks[i] += moving
+        for s in stacks:
+            print(s)
+    print("Part2:", "".join([i[-1] for i in stacks]))
 
 
 
