ec8d475eccd29282e628c8c87aac6a01ae9e1798
Trenton Hoffman <trenton.hoffman@elemica.com>
2022-12-07 14:30:05 -0500

    day6 complete

---
+++ day06/day06.py
@@ -0,0 +1,18 @@
+with open('input.txt') as input_data:
+    signal = ""
+    for line in input_data.readlines():
+        signal += line
+
+
+def get_marker(signal, n=4):
+    for i in range(n, len(signal)):
+        if len(set(signal[i-n:i])) == n:
+            return i
+
+print("Part1:", get_marker(signal, 4))
+
+
+print("Part2:", get_marker(signal, 14))
+
+
+
